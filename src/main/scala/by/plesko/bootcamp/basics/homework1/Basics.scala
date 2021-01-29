e69212703ca92f2ff821b3325f177dc9445784b8
VictorPlesko <victorplesko@gmail.com>
2021-01-29 15:14:42 +0300

    Wrote the gcd function

--- src/main/scala/by/plesko/bootcamp/basics/homework1/Basics.scala
+++ src/main/scala/by/plesko/bootcamp/basics/homework1/Basics.scala
@@ -1,7 +1,12 @@
 package by.plesko.bootcamp.basics.homework1
 
+import scala.annotation.tailrec
+
 object Basics {
   def lcm(a: Int, b: Int): Int = ???
 
-  def gcd(a: Int, b: Int): Int = ???
+  @tailrec
+  def gcd(a: Int, b: Int): Int =
+    if (b == 0) Math.abs(a)
+    else gcd(b, a % b)
 }

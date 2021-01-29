cdce1bc64f11738ba61aa49cafa4e69beaf9cb74
VictorPlesko <victorplesko@gmail.com>
2021-01-29 14:40:28 +0300

    Create new project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Homework"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
---
+++ src/main/scala/by/plesko/bootcamp/basics/homework1/Basics.scala
@@ -0,0 +1,7 @@
+package by.plesko.bootcamp.basics.homework1
+
+object Basics {
+  def lcm(a: Int, b: Int): Int = ???
+
+  def gcd(a: Int, b: Int): Int = ???
+}

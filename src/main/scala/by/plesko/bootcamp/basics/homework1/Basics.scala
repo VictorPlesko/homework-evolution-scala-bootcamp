0124ff745c47119332ac9438be4377b4c305c4f1
VictorPlesko <victorplesko@gmail.com>
2021-01-29 15:45:33 +0300

    Added behavior with zero

--- src/main/scala/by/plesko/bootcamp/basics/homework1/Basics.scala
+++ src/main/scala/by/plesko/bootcamp/basics/homework1/Basics.scala
@@ -3,10 +3,14 @@ package by.plesko.bootcamp.basics.homework1
 import scala.annotation.tailrec
 
 object Basics {
-  def lcm(a: Int, b: Int): Int = Math.abs(a * b) / gcd(a, b)
+  def lcm(a: Int, b: Int): Option[Int] =
+    if (a == 0 || b == 0) None
+    else Some(Math.abs(a * b) / gcd(a, b).get)
 
   @tailrec
-  def gcd(a: Int, b: Int): Int =
-    if (b == 0) Math.abs(a)
+  def gcd(a: Int, b: Int): Option[Int] = {
+    if (a == 0 && b == 0) None
+    else if (b == 0) Some(Math.abs(a))
     else gcd(b, a % b)
+  }
 }

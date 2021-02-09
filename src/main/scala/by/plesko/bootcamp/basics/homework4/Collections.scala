37e10f4ab4344cfd25e7330ed37f725662404dae
VictorPlesko <victorplesko@gmail.com>
2021-02-09 15:17:56 +0300

    Added the fourth task from homework 4

--- src/main/scala/by/plesko/bootcamp/basics/homework4/Collections.scala
+++ src/main/scala/by/plesko/bootcamp/basics/homework4/Collections.scala
@@ -1,15 +1,26 @@
 package by.plesko.bootcamp.basics.homework4
 
 object Collections {
+
+  // https://leetcode.com/problems/running-sum-of-1d-array/
   def runningSum(nums: Array[Int]): Array[Int] = {
     nums.foldLeft(Array.empty[Int])((acc, x) => acc :+ x + acc.lastOption.getOrElse(0))
   }
 
+  // https://leetcode.com/problems/shuffle-the-array
   def shuffle(nums: Array[Int], n: Int): Array[Int] = {
     nums.indices.take(n).flatMap(i => Array(nums(i), nums(i + n))).toArray
   }
 
+  // https://leetcode.com/problems/richest-customer-wealth
   def maximumWealth(accounts: Array[Array[Int]]): Int = {
     accounts.foldLeft(Int.MinValue)((maxWealth, acc) => maxWealth max acc.sum)
   }
+
+  // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+  def kidsWithCandies(candies: Array[Int], extraCandies: Int): Array[Boolean] = {
+    candies.foldLeft(Array.empty[Boolean])((res, candy) =>
+      if (candies.forall(_ <= candy + extraCandies)) res :+ true else res :+ false)
+  }
+
 }

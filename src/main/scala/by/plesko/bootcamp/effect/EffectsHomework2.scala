9b603f76cb32fa2580c257952b0544bfc56c51f8
VictorPlesko <victorplesko@gmail.com>
2021-04-09 15:08:17 +0300

    Refactor code in EffectsHomework2.scala

--- src/main/scala/by/plesko/bootcamp/effect/EffectsHomework2.scala
+++ src/main/scala/by/plesko/bootcamp/effect/EffectsHomework2.scala
@@ -8,7 +8,7 @@ import cats.implicits._
 import java.io.File
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContext
-import scala.io.{Source, StdIn}
+import scala.io.{BufferedSource, Source, StdIn}
 
 object EffectsHomework2 extends IOApp {
 
@@ -16,7 +16,7 @@ object EffectsHomework2 extends IOApp {
     def toString: String
   }
 
-  object Error {
+  object FileError {
     final case object NotFile extends FileError {
       override def toString: String = "The file path does not point to a file!"
     }
@@ -40,56 +40,78 @@ object EffectsHomework2 extends IOApp {
     }
   }
 
-  private def checkFilePath(filePath: String): Either[FileError, Unit] = {
-    val file = new File(filePath)
-    if (!file.exists) Left(Error.NotExists)
-    else if (!file.isFile) Left(Error.NotFile)
-    else if (!file.canRead) Left(Error.NotReadable)
-    else Right()
-  }
-
   def readSeed(blocker: Blocker)(implicit contextShift: ContextShift[IO]): IO[Int] =
     blocker
       .blockOn(IO(println("Enter seed: ")) *> IO(StdIn.readInt()))
       .handleErrorWith(_ => IO(println("Not a number!")) *> readSeed(blocker))
 
-  def javaHash(word: String, seed: Int = 0): Int = {
-    var hash = 0
-
-    for (ch <- word.toCharArray)
-      hash = 31 * seed * hash + ch.toInt
-
-    hash = hash ^ (hash >> 20) ^ (hash >> 12)
-    hash ^ (hash >> 7) ^ (hash >> 4)
+  private def checkFilePath(filePath: String): Either[FileError, Unit] = {
+    val file = new File(filePath)
+    if (!file.exists) Left(FileError.NotExists)
+    else if (!file.isFile) Left(FileError.NotFile)
+    else if (!file.canRead) Left(FileError.NotReadable)
+    else Right()
   }
 
-  def knuthHash(word: String, constant: Int): Int = {
-    var hash = 0
-    for (ch <- word.toCharArray)
-      hash = ((hash << 5) ^ (hash >> 27)) ^ ch.toInt
-    hash % constant
+  object Hash {
+    def javaHash(word: String, seed: Int = 0): Int = {
+      var hash = 0
+
+      for (ch <- word.toCharArray)
+        hash = 31 * seed * hash + ch.toInt
+
+      hash = hash ^ (hash >> 20) ^ (hash >> 12)
+      hash ^ (hash >> 7) ^ (hash >> 4)
+    }
+
+    def knuthHash(word: String, constant: Int): Int = {
+      var hash = 0
+      for (ch <- word.toCharArray)
+        hash = ((hash << 5) ^ (hash >> 27)) ^ ch.toInt
+      hash % constant
+    }
   }
 
   trait Repo[F[_]] {
     def add(filePath: String, minHash: Int): F[Unit]
     def get(filePath: String): F[Int]
   }
-  final case class MyRepo[F[_]: Functor](
-  private val repo: Ref[F, Map[String, Int]]
-  ) extends Repo[F] {
+
+  final case class MyRepo[F[_] : Functor](
+                                           private val repo: Ref[F, Map[String, Int]]
+                                         ) extends Repo[F] {
     override def add(filePath: String, minHash: Int): F[Unit] = repo.update(_ + (filePath -> minHash))
 
-    override def get(filePath: String): F[Int] = repo.get.map(_(filePath))
+    override def get(filePath: String): F[Int] = repo.get.map(_ (filePath))
   }
+
   object MyRepo {
-    def emptyRepo[F[_]: Sync]: F[MyRepo[F]] =
-      Ref.of[F,Map[String,Int]](Map.empty[String, Int]).map(ref => MyRepo(ref))
+    def emptyRepo[F[_] : Sync]: F[MyRepo[F]] =
+      Ref.of[F, Map[String, Int]](Map.empty[String, Int]).map(ref => MyRepo(ref))
+  }
+
+  def calculateSignature(filePath: String, descriptor: BufferedSource, seed: Int): IO[Unit] = {
+    for {
+      data <- IO(descriptor.getLines().toList)
+      words = data.flatMap(_.split("[\\p{Space}\\p{Punct}]").filter(_ != ""))
+      javaHashFiber <- IO.pure(words.map(word => Hash.javaHash(word, seed)).min).start
+      knuthHashFiber <- IO.pure(words.map(word => Hash.knuthHash(word, seed)).min).start
+      javaMinHash <- javaHashFiber.join
+      knuthMinHash <- knuthHashFiber.join
+      minHash = javaMinHash min knuthMinHash
+      myRepo <- MyRepo.emptyRepo[IO]
+      _ <- myRepo.add(filePath, minHash)
+      test <- myRepo.get(filePath)
+      _ <- IO(println(s"FilePath: $filePath, MinHash: $test"))
+    } yield ()
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
+    val executors = Executors.newFixedThreadPool(3)
+    val executionContext = ExecutionContext.fromExecutor(executors)
+    val blocker = Blocker.liftExecutionContext(executionContext)
+
     val resources = for {
-      executor <- Resource.make(IO(Executors.newFixedThreadPool(3)))(ex => IO(ex.shutdown()))
-      blocker = Blocker.liftExecutionContext(ExecutionContext.fromExecutor(executor))
       filePath <- Resource.liftF(readFilePath(blocker))
       seed <- Resource.liftF(readSeed(blocker))
       descriptor <- Resource.fromAutoCloseable(IO(Source.fromFile(filePath)))
@@ -97,22 +119,7 @@ object EffectsHomework2 extends IOApp {
 
     resources.use {
       case (filePath, descriptor, seed) =>
-          for {
-            data <- IO(descriptor.getLines().toList)
-            words = data.flatMap(_.split("[\\p{Space}\\p{Punct}]").filter(_ != ""))
-            _ <- IO(println(words))
-            javaHashFiber <- IO.pure(words.map(word => javaHash(word, seed)).min).start
-            knuthHashFiber <- IO.pure(words.map(word => knuthHash(word, seed)).min).start
-            javaMinHash <- javaHashFiber.join
-            knuthMinHash <- knuthHashFiber.join
-            _ <- IO(println(s"java: $javaMinHash, knuth: $knuthMinHash"))
-            minHash = javaMinHash min knuthMinHash
-            myRepo <- MyRepo.emptyRepo[IO]
-            _ <- myRepo.add(filePath, minHash)
-            test <- myRepo.get(filePath)
-            _ <- IO(println(s"FilePath: $filePath, MinHash: $minHash"))
-          } yield ()
-
-    } as ExitCode.Success
+        contextShift.evalOn(executionContext)(calculateSignature(filePath, descriptor, seed))
+    } *> IO(executors.shutdown()) *> IO(ExitCode.Success)
   }
 }
